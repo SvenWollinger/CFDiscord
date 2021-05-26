a817d7434a2cd769242eb5e2a9b13b07e227c40d
SvenWollinger <51021531+SvenWollinger@users.noreply.github.com>
2021-05-26 02:50:07 +0200

    Added uuid utils method

--- src/main/java/io/wollinger/cfdiscord/Utils.java
+++ src/main/java/io/wollinger/cfdiscord/Utils.java
@@ -2,9 +2,17 @@ package io.wollinger.cfdiscord;
 
 import org.apache.commons.lang.RandomStringUtils;
 
+import java.math.BigInteger;
+import java.util.UUID;
+
 public class Utils {
 
     public static String generateToken(int length, boolean useLetters, boolean useNumbers) {
         return RandomStringUtils.random(length, useLetters, useNumbers);
+    }
+
+    public static UUID stringToUUID(String uuid) {
+        String uuidString = uuid.replace("-", "");
+        return new UUID(new BigInteger(uuidString.substring(0, 16), 16).longValue(), new BigInteger(uuidString.substring(16), 16).longValue());
     }
 }
